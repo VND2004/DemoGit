29cbc704c1c049cdc8501983aba334ad3348f86f
VND2004 <vunhuduc2004@gmail.com>
2023-08-15 00:27:54 +0700

    init project

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+float n;
+string s;
+int main()
+{
+    cin >> n;
+    s = (n >= 8) ? "gioi" : (n >= 6.5 ? "kha" : (n >= 5 ? "Trung binh" : "yeu"));
+    cout << s;
+}
